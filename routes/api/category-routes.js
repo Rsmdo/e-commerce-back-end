54cc4778231952bf0ff576a9d329b5833c6cccd5
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-05-18 12:41:13 -0400

    diud get routw

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,32 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  //findall function 
+  Category.findAll({
+    //to get associated products 
+    include: {
+      model: Product, 
+      //selected all needed categoeries
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  //take the data and pass
+  .then((dbCatData) => {
+    //if no data found return a 404 (no cat found)
+    if (!dbCatData) {
+      res.status(404)
+      //may change this to display message in json
+      console.log("no categories found")
+      return;
+    }
+    res.json(dbCatData);
+
+  })
+  //if error
+  .catch(err => {
+    res.status(500).json(err); 
+    
+  });
 });
 
 router.get('/:id', (req, res) => {
