08ccf5c8cf771c1bfb8c1e3e5adc78db19e0bf9f
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-05-18 13:12:34 -0400

    finished product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,71 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+
+  //use finall again 
+  Product.findAll({
+    //using all need categories to include 
+    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    include: [
+      {
+        //includingn category data 
+        model: Category,
+        attributes: ["id", "category_name"],
+      },
+      {
+        //including tag data 
+        model: Tag,
+        through: ProductTag,
+        as: "tags",
+      },
+    ],
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  //use find one same as findall except different sub find one, where w params and if statment
+    Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        //including category data 
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        //including tag data 
+        model: Tag,
+        through: ProductTag,
+        as: "tags",
+      },
+    ],
+  })
+    .then((dbProductData) => {
+      //if the id searched is not found 
+      if (!dbProductData) {
+        res.status(404)
+        //returns this can be changed later 
+        console.log("no product found")
+        return;
+      }
+      res.json(dbProductData);
+    })
+    //if error 
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
 // create new product
@@ -91,6 +150,25 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  //using destroy again 
+  Product.destroy({
+    where: {
+      //to find exact id to delete
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    //if product to destroy is not found
+    if (!dbProductData) {
+      rs.status(404)
+      console.log("no product found")
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
-
 module.exports = router;
