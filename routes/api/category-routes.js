0c969ca48c2c510e0d9f6dab89a23b448fe9814e
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-05-18 12:52:10 -0400

    finshed category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,18 +37,102 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  //useing findone 
+  Category.findOne({
+    where: {
+      //to find specific produc need id 
+      id: req.params.id
+    },
+    include: {
+      //cat needed to include with request
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then((dbCatData) => {
+    //if no data found return a 404 (no cat found)
+    if (!dbCatData) {
+      res.status(404)
+      //may change this to display message in json
+      console.log("no categories found")
+      return;
+    }
+    res.json(dbCatData);
+
+  })
+  //if error
+  .catch(err => {
+    res.status(500).json(err); 
+    
+  });
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
+  //use create 
+  Category.create({
+    //creating a new cat
+    category_name: req.body.category_name
+  })
+    .then(dbCatData => res.json(dbCatData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+    // update a category by its `id` value
+    Category.update(req.body, {
+      where: {
+        //find correct cat to update
+        id: req.params.id
+      }
+    })
+    .then((dbCatData) => {
+      //if no data found return a 404 (no cat found)
+      if (!dbCatData) {
+        res.status(404)
+        //may change this to display message in json
+        console.log("no categories found")
+        return;
+      }
+      res.json(dbCatData);
+  
+    })
+    //if error
+    .catch(err => {
+      res.status(500).json(err); 
+      
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  //use destroy not delete
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((dbCatData) => {
+    //if no data found return a 404 (no cat found)
+    if (!dbCatData) {
+      res.status(404)
+      //may change this to display message in json
+      console.log("no categories found")
+      return;
+    }
+    res.json(dbCatData);
+
+  })
+  //if error
+  .catch(err => {
+    res.status(500).json(err); 
+    
+  });
 });
 
 module.exports = router;
